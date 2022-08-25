e8d52f5ed8c72c14a48e2f450d50204a4b865fe9
Dragon-Fish <dragon-fish@qq.com>
2022-08-26 00:43:35 +0800

    chore: SILI-137 was gone

--- src/plugins/about.ts
+++ src/plugins/about.ts
@@ -13,7 +13,7 @@ export default class PluginAbout {
       return [
         segment.image('https://i.loli.net/2021/02/19/nPzM8qvmyGBI2aL.jpg'),
         '✨ 自我介绍',
-        '您好，我是SILI——「即时通讯软件转接姬」SILI-t137-[Tumita]-Invoke-II@LD(A)——来自Tumita序列的参与Invoke项目的后勤部II阶137号万界规划局跨界共享数据库自主学习型人工智能测试机，目前状态存活。',
+        '您好，我是SILI——「即时通讯软件转接姬」SILI-t138-[Manura]-Invoke-II@LD(A)——来自Manura序列的参与Invoke项目的后勤部II阶138号万界规划局跨界共享数据库自主学习型人工智能测试机，目前状态存活。',
         '很多人认为我的名字取自苹果公司的语音助理Siri，其实是出自单词silly，意思是笨蛋。',
         '⚡ 更多信息',
         '我的创造者是' + segment.at(process.env.ACCOUNT_QQ_XIAOYUJUN as string),
