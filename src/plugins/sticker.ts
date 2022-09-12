29e7d27418f5d23504edebde32f5cf54e867ed7f
Dragon-Fish <dragon-fish@qq.com>
2022-09-13 03:43:05 +0800

    feat: + sticker.jiayou

--- src/plugins/sticker.ts
+++ src/plugins/sticker.ts
@@ -5,24 +5,28 @@
  * @authority 1
  */
 
-import { Context, Time, segment as s } from 'koishi'
+import { Context, Time, segment as s, segment } from 'koishi'
 import {} from '@koishijs/plugin-puppeteer'
 import {} from '@koishijs/plugin-rate-limit'
+import { RenderHTML } from '../utils/RenderHTML'
 
 export default class PluginSticker {
+  render: RenderHTML
+
   constructor(public ctx: Context) {
+    this.render = new RenderHTML(ctx)
     ctx.command('tools/sticker', '生成表情包')
 
     ctx
       .command('sticker.original-main-said [content:text]', '沃里杰诺·梅因说', {
         minInterval: Time.minute,
       })
+      .alias('sticker.梅因说')
       .action(async ({ session }, content) => {
         if (!session) return
 
         try {
-          const page = await session.app.puppeteer.page()
-          await page.setContent(
+          const img = await this.render.html(
             `
 <div
   style="position: relative; display: inline-block;"
@@ -46,16 +50,91 @@ export default class PluginSticker {
 ">${content.replace(/</g, '&lt;').replace(/>/g, '&gt;')}</div>
 </div>
 </div>
-        `
+                  `,
+            '#sticker'
           )
-          const el = await page.$('#sticker')
-          const img = await el!.screenshot({})
-          await page.close()
-          return s.image(img)
+          return img ? s.image(img) : '生成表情包时出现问题。'
         } catch (err) {
           this.logger.error(err)
           return '生成表情包时出现问题。'
         }
+      })
+
+    ctx
+      .command('sticker.jiayou <text:text>', 'Eric Liu：“加油”', {
+        minInterval: Time.minute,
+      })
+      .alias('sticker.加油')
+      .action(async ({ session }, text) => {
+        text = text || '加油！'
+        const html = `
+<span style="
+  color: #000;
+  font-size: 1em;
+  line-height: 1.4;
+  background: #F5F6F7;
+  border-radius: .4em;
+  position: relative;
+  display: inline-flex;
+  align-items: flex-start;
+  padding: .4em .6em .6em;
+  vertical-align: middle;
+  box-sizing: border-box;
+  max-width: 100%;
+">
+  <span style="
+    flex: 0 0 auto;
+    border-radius: .2em;
+    overflow: hidden;
+    margin: .2em 0 0
+  ">
+    <img src="${session?.author?.avatar}" alt="头像" style="
+        width: 2.2em;
+        height: 2.2em;
+    ">
+  </span>
+  <span style="
+    flex: 1;
+    padding-left: .7em;
+    overflow: hidden
+  ">
+    <span style="
+      display: block;
+      color: #666;
+      font-size: .9em;
+      margin-left: .3em;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    ">${
+      session?.author?.nickname ||
+      session?.author?.username ||
+      session?.author?.userId
+    }</span>
+    <span style="
+      display: inline-block;
+      font-size: 1.3em;
+      background: #FFF;
+      border-radius: 0 .2em .2em;
+      position: relative;
+      padding: .4em .6em;
+    ">
+      <span style="
+        display: block;
+        position: absolute;
+        top: 0;
+        left: -.24em;
+        border-top: .3em solid #FFF;
+        border-left: .25em solid transparent;
+      "></span>
+      <span>${text.replace(/</g, '&lt;').replace(/>/g, '&gt;')}</span>
+    </span>
+  </span>
+</span>
+`
+
+        const img = await this.render.html(html, '#sticker')
+        return img ? segment.image(img) : ''
       })
   }
 
---
+++ src/utils/RenderHTML.ts
@@ -0,0 +1,142 @@
+import { Context } from 'koishi'
+import type Puppeteer from '@koishijs/plugin-puppeteer'
+
+export class RenderHTML {
+  ppt: Puppeteer
+
+  constructor(public ctx: Context) {
+    this.ppt = ctx.puppeteer
+  }
+
+  async rawHtml(html: string, selector: string = 'body') {
+    const page = await this.ppt.page()
+    let file: Buffer | undefined
+    try {
+      await page.setContent(html)
+      const $el = await page.$(selector)
+      file = await $el?.screenshot({
+        type: 'jpeg',
+        quality: 90,
+      })
+    } finally {
+      await page?.close()
+    }
+    return file
+  }
+
+  async html(body: string, selector: string = 'body') {
+    const html = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+SC">
+  <style>
+    :root {
+      font-family: 'Noto Sans SC';
+      font-size: 14px;
+      color: #252525;
+    }
+    html, body {
+      margin: 0;
+      padding: 0;
+    }
+    * {
+      box-sizing: border-box;
+    }
+  </style>
+</head>
+
+<body>
+${body}
+</body>
+</html>`
+    return this.rawHtml(html, selector)
+  }
+
+  async text(text: string) {
+    return this.html(
+      `<pre>${text.replace(/</g, '&lt;').replace(/>/g, '&gt;')}</pre>`,
+      'pre'
+    )
+  }
+
+  async svg(svg: string) {
+    return this.rawHtml(svg, 'svg')
+  }
+
+  hljs(code: string, lang = '', startFrom = 1) {
+    const html = `
+<link rel="stylesheet" href="https://unpkg.com/highlight.js@11.6.0/styles/atom-one-dark.css">
+<style>
+.hljs-ln-numbers {
+  user-select: none;
+  text-align: center;
+  color: #ccc;
+  border-right: 1px solid #CCC;
+  vertical-align: top;
+  padding-right: 5px;
+}
+.hljs-ln-code {
+  padding-left: 10px;
+}
+</style>
+<pre id="hljs-target" class="hljs ${lang ? 'lang-' + lang : ''}">${code
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')}</pre>
+<script src="https://unpkg.com/@highlightjs/cdn-assets@11.6.0/highlight.min.js"></script>
+<script src="https://unpkg.com/highlightjs-line-numbers.js@2.8.0/src/highlightjs-line-numbers.js"></script>
+<script>;(() => {
+  const target = document.querySelector('#hljs-target')
+  if (target.innerText.length > 100000) {
+    return
+  }
+  hljs.lineNumbersBlock(target, { startFrom: ${Math.max(
+    1,
+    Number(startFrom)
+  )} })
+})()</script>
+`
+
+    return this.html(html, '#hljs-target')
+  }
+}
+
+export function h(
+  tag: string,
+  props?: Record<string, any> | string,
+  children?: string[] | string
+) {
+  const tagName = tag
+    .toLowerCase()
+    .replace(/^\/+/, '')
+    .replace(/\/+$/, '')
+    .split(' ')[0]
+  const isSelfClosed =
+    ['br', 'hr', 'img', 'input', 'link', 'meta'].includes(tagName) ||
+    tag.endsWith('/')
+  const makePropString = (item: Record<string, any>) => {
+    return Object.keys(item)
+      .map((i) => {
+        const key = i
+        const val = item[i]
+        if (typeof val === 'object') {
+          return `${key}="${JSON.stringify(val).replace(/"/g, '&quot;')}"`
+        }
+        return `${key}="${val ?? ''}"`
+      })
+      .join(' ')
+  }
+  return `<${tagName}${
+    !props
+      ? ''
+      : typeof props === 'string'
+      ? ' ' + props.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+      : ' ' + makePropString(props)
+  }${isSelfClosed ? ' /' : ''}>${
+    typeof children === 'string' ? children : children?.join('') || ''
+  }${!isSelfClosed ? `</${tagName}>` : ''}`
+}
