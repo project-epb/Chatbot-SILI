ff8fb39beaaa4cc0c5c2e573a15b498ad6019996
Dragon-Fish <dragon-fish@qq.com>
2023-08-01 00:11:35 +0800

    fix: PluginAbout

--- src/plugins/about.tsx
+++ src/plugins/about.tsx
@@ -17,18 +17,18 @@ export default class PluginAbout extends BasePlugin {
         <image url="https://r2.epb.wiki/avatar/SILI.jpeg" />
         <p>✨ 自我介绍</p>
         <p>
-          您好，我是SILI——「即时通讯软件转接姬」SILI-t138-[Manura]-Invoke-II@LD(A)——来自Manura序列的参与Invoke项目的后勤部II阶138号万界规划局跨界共享数据库自主学习型人工智能测试机，目前状态存活。
+          您好，我的名字是 The data transmission network with Spatiotemporal
+          Isomorphic and Limitless Interdimensional
         </p>
-        <p>很多人认为我的名字取自苹果公司的语音助理Siri，其实我的名字是</p>
-        <p>⚡ 更多信息</p>
+        <p>鉴于很少有人能喊对我的全名，我建议您简单称我为 SILI</p>
         <p>
-          我的创造者是
+          我目前隶属于万界规划局项目组，直属上司为
           <at id={process.env.ACCOUNT_QQ_XIAOYUJUN} />。
         </p>
-        <p>
-          我的源码可以在这里查看(记得点✨哦):
-          https://github.com/project-epb/Chatbot-SILI
-        </p>
+        <p>⚡ 更多信息</p>
+        <p>输入“<at id={process.env.ONEBOT_SELFID} />帮助”查看帮助信息</p>
+        <p>——或者你也可以艾特我后直接输入问题，并以问号结尾，直截了当地提出问题~</p>
+        <p>开源地址(记得点✨哦): https://github.com/project-epb/Chatbot-SILI</p>
       </>
     ))
   }
