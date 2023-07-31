020d1bd090222b05907b9108c0a75feebf619756
Dragon-Fish <dragon-fish@qq.com>
2023-07-31 22:52:13 +0800

    fix: PluginProfile

--- src/plugins/profile.ts
+++ src/plugins/profile.ts
@@ -13,21 +13,19 @@ export default class PluginProfile {
     ctx
       .command('admin/profile', '基本资料', {})
       // @ts-ignore
-      .userFields([
-        'id',
-        'authority',
-        'name',
-        'onebot',
-        'discord',
-        'qqguild',
-        'github.accessToken',
-      ])
-      .action(({ session }) => {
+      .userFields(['id', 'authority', 'name', 'github.accessToken'])
+      .action(async ({ session }) => {
         if (!session) return
         const nickname = session.user?.name
           ? segment.escape(session.user?.name)
           : ''
-        const isBound = (i: boolean) => (i ? '√ 已绑定' : '× 未绑定')
+        const bindings = await session.app.database.get('binding', {
+          aid: session.user.id,
+        })
+        const isBoundText = (i: boolean) => (i ? '√ 已绑定' : '× 未绑定')
+        const isPlatformBoundText = (platform: string) => {
+          return isBoundText(!!bindings.find((i) => i.platform === platform))
+        }
         const msgBuilder = new BulkMessageBuilder(session)
         msgBuilder
           .prependOriginal()
@@ -42,14 +40,14 @@ export default class PluginProfile {
           .botSay(
             [
               `账号绑定情况:`,
+              `${isPlatformBoundText('onebot')} QQ`,
+              `${isPlatformBoundText('qqguild')} QQ频道`,
+              `${isPlatformBoundText('discord')} Discord`,
+              `${isPlatformBoundText('kooka')} Kook`,
+              `${isPlatformBoundText('villa')} 大别野`,
+              `${isPlatformBoundText('dingtalk')} 钉钉`,
               // @ts-ignore
-              `${isBound(!!session.user?.onebot)} QQ`,
-              // @ts-ignore
-              `${isBound(!!session.user?.qqguild)} QQ频道`,
-              // @ts-ignore
-              `${isBound(!!session.user?.discord)} Discord`,
-              // @ts-ignore
-              `${isBound(!!session.user?.github?.accessToken)} GitHub`,
+              `${isBoundText(!!session.user?.github?.accessToken)} GitHub`,
             ].join('\n')
           )
         return msgBuilder.all()
