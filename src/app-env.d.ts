f7c1651e339207486f5a73290112f1d018a9f613
Dragon-Fish <dragon-fish@qq.com>
2023-08-12 05:55:57 +0800

    chore: housekeeping

--- src/app-env.d.ts
+++ src/app-env.d.ts
@@ -1,4 +1,4 @@
-import type { Context, DatabaseService } from 'koishi'
+import type { Context } from 'koishi'
 import type { Mint } from 'mint-filter'
 import type { HTMLService } from './utils/RenderHTML'
 import type {} from '@koishijs/plugin-database-mongo'
@@ -6,9 +6,8 @@ import type {} from '@koishijs/plugin-help'
 import type {} from '@koishijs/plugin-rate-limit'
 import type {} from 'koishi-plugin-switch'
 
-declare module 'koishi' {
+declare module '@koishijs/core' {
   export interface Context {
-    database: DatabaseService
     html: HTMLService
     mint: Mint
   }
