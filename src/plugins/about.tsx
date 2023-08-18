dc975797a1cbcffabff707b9d40840fa60b3eb99
Dragon-Fish <dragon-fish@qq.com>
2023-08-18 17:06:42 +0800

    chore: housekeeping

--- src/plugins/about.tsx
+++ src/plugins/about.tsx
@@ -3,6 +3,7 @@ import BasePlugin from './_boilerplate'
 
 export default class PluginAbout extends BasePlugin {
   #masterUid = 1
+  #masterName = '机智的小鱼君'
   #masterPidCache: Map<string, string> = new Map()
 
   constructor(public ctx: Context) {
@@ -12,7 +13,7 @@ export default class PluginAbout extends BasePlugin {
       .command('about', '自我介绍')
       .alias('自我介绍', '关于')
       .action(async ({ session }) => {
-        const masterPid = await this.getMasterPid(session as Session)
+        const masterPid = await this.getMasterPlatformId(session as Session)
         return (
           <>
             <image url="https://r2.epb.wiki/avatar/SILI.jpeg" />
@@ -24,12 +25,17 @@ export default class PluginAbout extends BasePlugin {
             <p>鉴于很少有人能喊对我的全名，我建议您简单称我为 SILI</p>
             <p>
               我目前隶属于万界规划局项目组，直属上司为
-              {masterPid ? <at id={masterPid} name='机智的小鱼君' /> : '@机智的小鱼君'}。
+              {masterPid ? (
+                <at id={masterPid} name={this.#masterName} />
+              ) : (
+                `@${this.#masterName}`
+              )}
+              。
             </p>
             <p>⚡ 更多信息</p>
             <p>
               输入“
-              <at id={session.selfId} />
+              <at id={session.selfId} name="SILI" />
               帮助”查看帮助信息
             </p>
             <p>
@@ -43,19 +49,19 @@ export default class PluginAbout extends BasePlugin {
       })
   }
 
-  async getMasterPid(session: Session) {
+  async getMasterPlatformId(session: Session) {
     if (this.#masterPidCache.has(session.platform)) {
       return this.#masterPidCache.get(session.platform)
     }
-    const [xyj] = await session.app.database
+    const [binding] = await session.app.database
       .get('binding', {
         aid: this.#masterUid,
         platform: session.platform,
       })
       .catch(() => [] as Binding[])
-    if (xyj) {
-      this.#masterPidCache.set(session.platform, xyj.pid)
-      return xyj.pid
+    if (binding) {
+      this.#masterPidCache.set(session.platform, binding.pid)
+      return binding.pid
     } else {
       return ''
     }
