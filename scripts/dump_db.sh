4d40220d9465e013fa1ad3905916f5245ff6610f
dragon-fish <dragon-fish@qq.com>
2025-02-06 04:13:16 +0800

    chore: update scripts

--- scripts/dump_db.sh
+++ scripts/dump_db.sh
@@ -8,11 +8,11 @@ db=${1:-"sili_v4"}
 # 获取脚本所在的目录，并找到 ../backup 目录的绝对路径
 script_path=$(realpath "$0")
 script_dir=$(dirname "$script_path")
-mkdir -p "$script_dir/../.backups/mongo_dump"
-backup_dir=$(realpath "$script_dir/../.backups/mongo_dump")
+mkdir -p "$script_dir/../.backups"
+backup_dir=$(realpath "$script_dir/../.backups")
 
 # 获取当前日期时间
 cur_date=$(date +"%Y%m%d%H%M%S")
 
 # 执行 MongoDB 备份
-docker exec -i sili-mongo bash -c "mongodump --db $db --archive --gzip" > "$backup_dir/$db-$cur_date.gz"
+docker exec -i sili-mongo bash -c "mongodump --db $db --archive --gzip" > "$backup_dir/mongo_dump-$db-$cur_date.archive.gz"
