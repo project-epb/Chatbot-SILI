1634a4c20d45fe6607eafb427ed27088279d9a55
dragon-fish <dragon-fish@qq.com>
2025-03-06 20:13:41 +0800

    build: drop ipv4_address, add mem_limit

--- src/modules/GuildRequestFirewall.ts
+++ src/modules/GuildRequestFirewall.ts
@@ -3,14 +3,14 @@ import { Context } from 'koishi'
 import BasePlugin from '@/plugins/_boilerplate'
 
 export class GuildRequestFirewall extends BasePlugin {
-  static readonly ANSWER_BLACK_LIST = ['管理员你好，我是来交流学习的']
+  static readonly ANSWER_BLACK_LIST = ['交流学习', '通过一下', '同意一下']
 
   constructor(ctx: Context) {
     super(ctx, null, 'guild-request-firewall')
 
-    ctx.guild().on('guild-member-request', async (session) => {
+    ctx.channel().on('guild-member-request', async (session) => {
       const answer = session.content.split('答案：').pop().trim()
-      if (GuildRequestFirewall.ANSWER_BLACK_LIST.includes(answer)) {
+      if (GuildRequestFirewall.ANSWER_BLACK_LIST.some((i) => answer.includes(i))) {
         await session.bot.handleGuildMemberRequest(session.messageId, false, '')
         session.sendQueued('提示：SILI 拒绝了一条烦人的入群申请。')
       }
