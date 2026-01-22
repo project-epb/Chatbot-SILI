aad46914b56da3a9bcd0e471eccf34b3c9957e89
dragon-fish <dragon-fish@qq.com>
2026-01-22 11:50:36 +0800

    fix: improve mediawiki login process

--- src/plugins/mediawiki/utils.ts
+++ src/plugins/mediawiki/utils.ts
@@ -1,89 +1,79 @@
-import type { CookieJarItem } from 'fexios/plugins'
 import { MediaWikiApi } from 'wiki-saikou/node'
 
+const MW_CLIENTS = new Map<string, MediaWikiApi>()
 const MOCK_UA =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'
-
-const COOKIE_STORE = new Map<string, CookieJarItem[]>()
+const MW_ACCOUNTS: {
+  match: (url: string) => boolean
+  username: string
+  password: string
+}[] = [
+  {
+    match: (url: string) => url.includes('.moegirl.org.cn'),
+    username: process.env.MW_BOTPASSWORD_MOEGIRL_USERNAME,
+    password: process.env.MW_BOTPASSWORD_MOEGIRL_PASSWORD,
+  },
+]
+const MOCK_HEADER = [
+  {
+    match: (url: string) => url.includes('huijiwiki.com'),
+    headers: {
+      'User-Agent': MOCK_UA,
+    },
+  },
+  {
+    match: (url: string) => url.includes('ngnl.wiki'),
+    headers: {
+      'User-Agent': MOCK_UA,
+    },
+  },
+  {
+    match: (url: string) => url.includes('.moegirl.org.cn'),
+    headers: {
+      'User-Agent': process.env.TOKEN_MOEGIRL_USER_AGENT,
+    },
+  },
+]
 
 export async function useApi(baseURL: string): Promise<MediaWikiApi> {
-  const USE_MOCK_HEADER = [
-    {
-      match: (url: string) => url.includes('huijiwiki.com'),
-      headers: {
-        'User-Agent': MOCK_UA,
-      },
-    },
-    {
-      match: (url: string) => url.includes('ngnl.wiki'),
-      headers: {
-        'User-Agent': MOCK_UA,
-      },
-    },
-    {
-      match: (url: string) => url.includes('.moegirl.org.cn'),
-      headers: {
-        'User-Agent': process.env.TOKEN_MOEGIRL_USER_AGENT,
-      },
-    },
-  ]
-  const USE_AUTHORIZATION: {
-    match: (url: string) => boolean
-    username: string
-    password: string
-  }[] = [
-    {
-      match: (url: string) => url.includes('.moegirl.org.cn'),
-      username: process.env.MW_BOTPASSWORD_MOEGIRL_USERNAME,
-      password: process.env.MW_BOTPASSWORD_MOEGIRL_PASSWORD,
-    },
-  ]
+  if (MW_CLIENTS.has(baseURL)) {
+    return MW_CLIENTS.get(baseURL)!
+  }
 
   const api = new MediaWikiApi({ baseURL })
+  MW_CLIENTS.set(baseURL, api)
 
-  const mockHeaders = USE_MOCK_HEADER.find((i) => i.match(baseURL))
+  const mockHeaders = MOCK_HEADER.find((i) => i.match(baseURL))
   if (mockHeaders) {
     console.info('[MWAPI]', 'Use mock headers:', baseURL, mockHeaders.headers)
     api.config.fexiosConfigs.headers ||= {}
     Object.assign(api.config.fexiosConfigs.headers, mockHeaders.headers)
   }
 
-  const auth = USE_AUTHORIZATION.find((i) => i.match(baseURL))
+  const auth = MW_ACCOUNTS.find((i) => i.match(baseURL))
   if (auth && auth.username && auth.password) {
-    console.info('[MWAPI]', 'Authorization:', baseURL, auth.username)
-    const cookies = COOKIE_STORE.get(`${baseURL}#${auth.username}`)
-    if (cookies) {
-      console.info('[MWAPI]', 'By cookies:', baseURL, auth.username)
-      const items = COOKIE_STORE.get(`${baseURL}#${auth.username}`)
-      if (items) {
-        items.forEach((item) => {
-          api.request.cookieJar?.setCookie(item)
-        })
-      }
-    } else {
-      console.info('[MWAPI]', 'By login:', baseURL, auth.username)
-      await api
-        .login(auth.username, auth.password)
-        .then(() => {
-          return api.get<{ query: { userinfo: { id: number } } }>({
-            action: 'query',
-            meta: 'userinfo',
-          })
-        })
-        .then(({ data }) => {
-          if (data.query.userinfo.id > 0) {
-            console.info('[MWAPI]', 'Logged in:', baseURL, data.query.userinfo)
-            COOKIE_STORE.set(
-              `${baseURL}#${auth.username}`,
-              api.request.cookieJar?.getCookies()
-            )
-          }
-        })
-        .catch((e) => {
-          console.error('[MWAPI]', 'Login failed:', baseURL, auth.username, e)
-        })
-    }
+    await api
+      .login(
+        auth.username,
+        auth.password,
+        {},
+        { autoRelogin: true, autoReloginRetries: 3 }
+      )
+      .then(({ lgusername, lguserid }) => {
+        console.info('[MWAPI]', 'Login success:', baseURL, lgusername, lguserid)
+      })
+      .catch((err) => {
+        console.error(
+          '[MWAPI]',
+          'Login failed:',
+          baseURL,
+          auth.username,
+          err.message || err
+        )
+        MW_CLIENTS.delete(baseURL)
+      })
   }
+
   return api
 }
 
